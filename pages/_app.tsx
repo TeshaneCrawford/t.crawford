bce0647f166e3b0677d8e2226aed729b67361479
Teshane Crawford <crawfordteshane@gmail.com>
2022-08-03 10:18:07 -0500

    jsx pragma for nextjs setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+/** @jsxImportSource theme-ui */
+import { ThemeProvider } from 'theme-ui'
+import { theme } from '.././shared/util/Theme'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ shared/util/Theme/index.ts
@@ -0,0 +1,8 @@
+import type { Theme } from 'theme-ui'
+
+export const theme: Theme = {
+    colors: {
+        primary: '#0070f3',
+    },
+}
+
