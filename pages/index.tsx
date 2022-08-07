2ac6604bc69a00d287d087c6971a8a0031274f2c
Teshane Crawford <crawfordteshane@gmail.com>
2022-08-07 10:13:44 -0500

    chakra ui integration fully set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-
+import Code from '../components/Code'
 
 const Home: NextPage = () => {
   return (
     <>
       <main>
+        <Code />
         <h1>
           Hello Next.js, Lets goooo yehhhh!!
         </h1>
