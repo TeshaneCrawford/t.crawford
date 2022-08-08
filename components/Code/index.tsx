4c5bbbdd36739fc5c008341e8c4f9c35c0d3e0d6
Teshane Crawford <crawfordteshane@gmail.com>
2022-08-08 13:29:23 -0500

    Integrated colour schemes in scss files

--- components/Code/index.tsx
+++ components/Code/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Code, Stack } from '@chakra-ui/react'
 
+
 const index = () => {
     return (
         <div>
             <Stack direction='row'>
-                <Code children='console.log(welcome)' />
-                <Code colorScheme='red' children="const chakra = 'awesome!'" />
                 <Code colorScheme='yellow' children='yarn add chakra' />
             </Stack>
         </div>
