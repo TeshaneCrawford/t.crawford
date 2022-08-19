df0229e2ac33bbfa2731e0513a02caa0547c73fa
Teshane Crawford <crawfordteshane@gmail.com>
2022-08-19 12:43:33 -0500

    update tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 // import { Box } from '@chakra-ui/react'
 import Hero from '../Layout/Hero'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +11,9 @@ const Home: NextPage = () => {
       <main>
         <div>
           <Hero />
+          <button className="btn-primary">
+            Save changes
+          </button>
         </div>
       </main>
     </>
