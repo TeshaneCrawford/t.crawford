9e2f1233a45df2b3da0fb55c3afb150c488735e1
Teshane Crawford <crawfordteshane@gmail.com>
2022-09-10 12:44:03 -0500

    refactoring code

--- Layout/Head/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Head = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default Head
--- components/Card/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default Card
