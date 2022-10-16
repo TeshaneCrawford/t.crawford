c86ebc81059488d48a28f444659039a2a9cfadc3
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-15 23:31:31 -0500

    duplicate identifier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { DarkModeProvider } from 'context/DarkModeContext';
-
 import Layout from '../Layout';
 import { DarkModeProvider } from '../context/darkModeContext';
 
