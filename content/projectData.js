891d60688ff302a1720258f9f947d025f857f5d3
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-16 18:20:05 -0500

    update to projects screen

--- content/projectData.js
+++ content/projectData.js
@@ -1,13 +1,35 @@
 module.exports = [
     {
-      id: 15,
-      name: "Google Docs with Next.js",
-      coverURL: "https://imgur.com/bQkEGlb.png",
+      id: 14,
+      name: "Gericht - Restaurant App",
+      coverURL: "https://i.imgur.com/YVxPuI3.png",
       description:
-        "Next Google Docs is a web app which uses draft.js to create a document for you. It also uses Firebase to store all the user's data.",
-      githubURL: "https://github.com/j471n/next-google-docs",
-      previewURL: "https://google-next-docs.vercel.app/",
-      tools: ["Next.js", "Tailwind CSS", "Firebase", "Draft.js", "Next Auth"],
+        "Resturant with a Mexican vibe",
+      githubURL: "https://github.com/TeshaneCrawford/resturant-g",
+      previewURL: "https://devtesh-resturantg.netlify.app/",
+      tools: ["React.js", "CSS3", "Firebase", "Email.js", "Next Auth"],
       pinned: true,
     },
+    {
+      id: 13,
+      name: "Fundraiser For The Homeless",
+      coverURL: "https://i.imgur.com/oB9xtAb.png",
+      description:
+        "Landing Page for a fundraiser for the homeless",
+      githubURL: "https://github.com/TeshaneCrawford/fundrasier-global",
+      previewURL: "https://fundrasier-global.vercel.app/",
+      tools: ["Next.js", "Theme UI", "Firebase", "Google Analytics", "SendGrid", "Vercel", "Netlify"],
+      pinned: true,
+    },
+    // {
+    //   id: 12,
+    //   name: "Fundraiser For The Homeless",
+    //   coverURL: "https://imgur.com/bQkEGlb.png",
+    //   description:
+    //     "Landing Page for a fundraiser for the homeless",
+    //   githubURL: "https://github.com/TeshaneCrawford/fundrasier-global",
+    //   previewURL: "https://fundrasier-global.vercel.app/",
+    //   tools: ["Next.js", "Theme UI", "Firebase", "Google Analytics", "SendGrid", "Vercel", "Netlify"],
+    //   pinned: true,
+    // },
 ];
