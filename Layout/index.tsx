5403925f23c8a82ba78e9c5de3754c1d25a7e51e
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-15 23:42:38 -0500

    new import

--- Layout/index.tsx
+++ Layout/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Header from "./Header";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import TopNavigation from "../components/TopNavigation";
 
 export interface Props {
   // routes: Route[];
@@ -15,7 +16,7 @@ const Layout: React.FC<React.PropsWithChildren<Props>> = ({ children }) => {
   return (
     <React.Fragment>
       <Header />
-      <Navbar />
+      <TopNavigation />
       {/* hasNotify isHome routes={routes} */}
       <main>
         {children}
