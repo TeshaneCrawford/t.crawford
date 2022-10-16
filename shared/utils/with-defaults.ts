f4e637bb892631f9356987d568183e72dc5cf9f5
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-16 16:34:00 -0500

    remove props

--- shared/utils/with-defaults.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { HeaderProps } from "../../Layout/Header";
-
-const withDefaults = (component: React.FC<HeaderProps>, defaultProps: { description: string; image: string; }) => {
-  component.defaultProps = defaultProps;
-  return component;
-};
-export default withDefaults;
