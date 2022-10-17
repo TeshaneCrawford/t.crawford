c2d41a0244cba43bc415233f2ba58557a1698741
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-16 19:43:26 -0500

    update social links

--- content/socialMedia.js
+++ content/socialMedia.js
@@ -1,48 +1,43 @@
 import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
-import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaDev } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+// import { FaDev } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-import { SiCodepen } from "react-icons/si";
+// import { SiCodepen } from "react-icons/si";
 
 export default [
   {
     title: "Twitter",
     Icon: AiOutlineTwitter,
-    url: "https://twitter.com/intent/follow?screen_name=j471n_",
+    url: "https://twitter.com/_D_Shybrid",
   },
   {
     title: "LinkedIn",
     Icon: BsLinkedin,
-    url: "https://www.linkedin.com/in/j471n/",
+    url: "https://www.linkedin.com/in/teshanecrawford/",
   },
   {
     title: "Github",
     Icon: BsGithub,
-    url: "https://github.com/j471n",
+    url: "https://github.com/TeshaneCrawford",
   },
   {
     title: "Instagram",
     Icon: AiOutlineInstagram,
-    url: "https://www.instagram.com/j471n_",
+    url: "https://www.instagram.com/teshanecrawford/",
   },
-  {
-    title: "Dev.to",
-    Icon: FaDev,
-    url: "https://dev.to/j471n",
-  },
-  {
-    title: "Codepen",
-    Icon: SiCodepen,
-    url: "https://codepen.io/j471n",
-  },
-  {
-    title: "Facebook",
-    Icon: BsFacebook,
-    url: "https://www.facebook.com/ja7in/",
-  },
+  // {
+  //   title: "Dev.to",
+  //   Icon: FaDev,
+  //   url: "https://dev.to/j471n",
+  // },
+  // {
+  //   title: "Codepen",
+  //   Icon: SiCodepen,
+  //   url: "https://codepen.io/j471n",
+  // },
   {
     title: "Mail",
     Icon: HiMail,
-    url: "mailto:jatinsharma8669@gmail.com",
+    url: "mailto:crawfordteshane@gmail.com",
   },
 ];
