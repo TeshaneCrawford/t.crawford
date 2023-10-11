3b27fefc970f718a1612ce46006b35fb862c13cb
Teshane Crawford <crawfordteshane@gmail.com>
2023-10-11 13:12:33 -0500

    :tada: New project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Teshane Crawford | Software Engineer',
+  description: 'Teshane Crawford is a software engineer, problem solver, and lifelong learner.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { css } from '@/css';
+
+export default function Home() {
+  return (
+    <main className=''>
+      <div className={css({
+        fontSize: '3xl',
+        fontWeight: 'bold',
+      })}>My Portfolio</div>
+    </main>
+  );
+}
