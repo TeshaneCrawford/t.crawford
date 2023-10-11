977fb75c466c2ffd8c4c9bce99908bf2d7fe5941
Teshane Crawford <crawfordteshane@gmail.com>
2023-10-11 17:16:13 -0500

    :construction: Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { css } from '@/css';
+import {Container } from '@/jsx'
 
 export default function Home() {
   return (
-    <main className=''>
+    <Container py="10">
       <div className={css({
         fontSize: '3xl',
         fontWeight: 'bold',
-      })}>My Portfolio</div>
-    </main>
+      })}>My Portfolio Website</div>
+    </Container>
   );
 }
