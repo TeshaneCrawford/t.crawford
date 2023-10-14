776930d1e034a9f49f52c8ed6386e0373540f023
Teshane Crawford <crawfordteshane@gmail.com>
2023-10-14 18:05:16 -0500

    :construction: Adding styles

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+    socials: {
+        twitter: 'Teshane Crawford',
+        github: 'TeshaneCrawford/t.crawford'
+    },
+    nuxtIcon: {
+        aliases: {
+            'dark-mode': 'ph:moon-bold',
+            'light-mode': 'ph:sun-bold'
+        }
+    }
+})
---
+++ components/PortfolioLayout.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: PortfolioLayout
+  </div>
+</template>
+
+<style scoped></style>
