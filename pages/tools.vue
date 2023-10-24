089943c21a97e2fb6858eb54c0470e7ff13b1fdb
Teshane Crawford <crawfordteshane@gmail.com>
2023-10-24 16:04:42 -0500

    :construction: Update to tools page

--- pages/tools.vue
+++ pages/tools.vue
@@ -30,9 +30,18 @@ useHead({
       </h2>
       <div grid grid-cols-3 gap-3 mt-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6>
       <div v-for="(value) in tool.toolData" 
-      class="relative flex flex-col gap-3 items-center justify-center bg-transparent p-8 border border-gray-2 hover:border-gray-400 dark:hover:border-neutral-600 rounded-md transition-all lg:hover:!scale-110 active:!scale-80 hover:z-10 hover:origin-center text-gray-700 hover:text-black dark:text-gray-300/80 dark:hover:text-white"
+      b="~ black op-10"
+      dark-b="white op-10"
+      rounded-md
+      p-8
+      shadow-sm
+    trans
+    hover="bg-orange bg-op-3"
+    dark-hover="bg-white bg-op-3"
+    class="transition-all lg:hover:!scale-110 active:!scale-80 hover:z-10 hover:origin-center"
       >
         <NuxtLink v-if="value.link" :to="value.link" target="_blank" :title="value.label"
+        class=""
         >
           <icon v-if="value.icon" :name="value.icon" w-2.1em h-2.1em/>
           <p class="absolute text-[10px] select-none">
