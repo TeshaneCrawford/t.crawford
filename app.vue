35469f9bab32e38948da99e46897d16b94fd068c
Teshane Crawford <crawfordteshane@gmail.com>
2023-11-13 22:53:07 -0500

    Add new favicon and update app.vue with new links

--- app.vue
+++ app.vue
@@ -7,7 +7,12 @@ useHead({
     // { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
-    { rel: 'icon', type: 'image/png', href: '/icon.png' }
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+    { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+    { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+    { rel: "manifest", href: "/site.webmanifest" }
+
   ],
   htmlAttrs: {
     lang: 'en'
