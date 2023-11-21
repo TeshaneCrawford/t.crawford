91876362e83dc3e2bc1bda110fdea79fa5cf5875
Teshane Crawford <crawfordteshane@gmail.com>
2023-11-20 22:38:32 -0500

    Add social links to chat page

---
+++ components/IndicatorPill.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-baseline gap-3">
+    <div class="flex-none rounded-full p-1 text-green-500 bg-green-500/10">
+      <div class="h-1.5 w-1.5 rounded-full bg-current animate-pulse"></div>
+    </div>
+    <h2 class="text-sm font-normal text-gray-400">
+      Available for hire
+    </h2>
+  </div>
+</template>
---
+++ components/SocialLinks.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+interface SocialLinksProps {
+  links: {
+    name: string;
+    url: string;
+    icon: string;
+  }[];
+}
+
+
+const links = [
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/teshanecrawford/',
+    icon: 'i-ri-linkedin-box-line',
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/TeshaneCrawford",
+    icon: "i-ri-github-fill",
+  },
+  {
+    name: "Stack Overflow",
+    url: "https://stackoverflow.com/users/8859340/teshane-crawford",
+    icon: "i-ri-stack-overflow-line",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/_d_shybrid",
+    icon: "i-ri-twitter-x-line",
+  },
+  {
+    name: "Telegram",
+    url: "",
+    icon: "i-ri-telegram-line",
+  },
+  {
+    name: "Threads",
+    url: "https://www.threads.net/@teshanecrawford",
+    icon: "i-ri-threads-line",
+  }
+]
+</script>
+
+<template>
+  <div>
+    <h2>Find Me On</h2>
+    <div class="space-y-5">
+      <NuxtLink v-for="link in links" :key="link.icon" :to="link.url" target="_blank" external class="flex items-end gap-4 dark:hover:text-gray-300">
+        <span>{{ link.name }}</span>
+        <div
+          class="flex-1 border-b border-dashed border-gray-300 hover:border-gray-700 transition-colors"
+        ></div>
+        <div border border-rounded p-2 dark:border-base>
+          <icon class="w-6 h-6" :name="link.icon" />
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
