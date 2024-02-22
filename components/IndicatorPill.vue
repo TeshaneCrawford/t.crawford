364c9d5c1418e6a0eb00a0a621cc06d116a72a6f
Teshane Crawford <crawfordteshane@gmail.com>
2024-02-22 16:08:55 -0500

    Refactor IndicatorPill component and remove unused dependencies

--- components/IndicatorPill.vue
+++ components/IndicatorPill.vue
@@ -3,7 +3,7 @@
     <div class="flex-none rounded-full bg-green-500/10 p-1 text-green-500">
       <div class="h-1.5 w-1.5 animate-pulse rounded-full bg-current" />
     </div>
-    <h2 class="text-sm font-normal text-gray-400">
+    <h2 class="text-sm text-gray-400 font-normal">
       Available for hire
     </h2>
   </div>
