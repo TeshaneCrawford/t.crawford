aa9f84337de1f77b5b052e4117f88f8cb5f5ad44
Teshane Crawford <crawfordteshane@gmail.com>
2024-04-10 15:28:00 -0500

    Add @nuxt/content and related dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
   typescript: {
     typeCheck: true,
     strict: true,
   },
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/fonts", "@nuxtjs/seo"]
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxtjs/seo",
+    "@vueuse/nuxt",
+    "@nuxt/content"
+  ],
+
+  runtimeConfig: {
+    public: {
+      trailingSlash: true,
+      siteUrl: 'https://teshanecrawford.live',
+      siteName: 'Teshane Crawford',
+      siteDescription: 'Teshane Crawford is a software engineer based in New York City.',
+      language: 'en',
+    }
+  },
+
+  content: {
+    highlight: {
+      theme: {
+        default: 'material-theme-lighter',
+        dark: 'material-theme-palenight'
+      },
+    }
+  },
+
+  ui: {
+    icons: ['ri', 'logos', 'simple-icons']
+  }
 })
