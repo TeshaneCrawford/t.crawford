0f8e05463680307de3340e8d13f143f37c4ad563
Teshane Crawford <crawfordteshane@gmail.com>
2024-04-10 13:33:27 -0500

    Enable TypeScript type checking and strict mode in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
   devtools: { enabled: true }
 })
