4d26797dbbff55f25907908c00b171ad1b57f45f
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-04-20 15:48:21 -0500

    Update home page SEO

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <script lang="ts" setup>
-const { data: doc } = await useAsyncData('home', () => queryContent('/').findOne())
+const { data: doc, error } = await useAsyncData('index', () => queryContent('/').findOne());
+
+if (error.value) {
+  throw createError({
+    statusCode: 404,
+    message: error.value.message || 'Page not found',
+    fatal: true,
+  })
+}
 
 const title = 'Teshane Crawford'
 
 useSeoMeta({
-  title: null,
+  titleTemplate: '%siteName| %pageTitle',
+  title: 'Software Engineer',
   ogTitle: title,
+  description: doc.value?.description,
+})
+
+defineOgImageComponent('OgImagePage', {
+  title: 'Teshane Crawford',
+  illustration: ''
 })
 </script>
 
