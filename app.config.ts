3962c20fabfb41ddebbfb96c1d7702a983974c21
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-04-20 18:02:38 -0500

    Update home page with new hero section, SEO improvements, and UI enhancements

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,15 @@ export default defineAppConfig({
     primary: 'cyan',
     gray: 'cool',
     button: {
-      base: 'transition ease-in'
+      base: 'transition ease-in',
+      color: {
+        gray: {
+          solid: 'shadow-none bg-gray-300/20 hover:bg-gray-300/40 dark:bg-gray-700/40 dark:hover:bg-gray-700/50',
+        },
+      },
+      variant: {
+        solid: 'shadow-none',
+      },
     },
     icons: {
       height: '1.2em',
--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -10,46 +10,51 @@ defineProps<{
 
 const subtileLinks = [{
   label: 'Software Engineer',
-  icon: 'i-twemoji-rocket'
 },
 {
   label: 'Tech Enthusiast',
-  icon: 'i-twemoji-technologist'
 },
 {
   label: 'Problem Solver',
-  icon: 'i-twemoji-gear',
 },
 ]
 </script>
 
 <template>
-  <section class="flex justify-center flex-col gap-5">
-    <h1 class="text-4xl font-bold tracking-tight text-gray-800 dark:text-gray-100 sm:text-6xl text-center">{{ title }}</h1>
+  <section class="flex justify-center flex-col gap-2">
+    <h1 class="text-4xl font-bold tracking-tight text-gray-800 dark:text-gray-100 sm:text-7xl text-center">{{ title }}</h1>
     <sapn class="flex justify-center">
       <UBreadcrumb
       :ui="{
-        ol: 'gap-x-6'
-      }" :links="subtileLinks" />
+        ol: 'gap-x-3',
+      }" :links="subtileLinks">
+      <template #default="{ isActive, link }">
+        <span :class="isActive ? 'text-gray-500 dark:text-gray-400' : 'text-gray-500 dark:text-gray-400'">{{ link.label }}</span>
+      </template>
+      </UBreadcrumb>
     </sapn>
-    <p class="text-center">{{ description }}</p>
+    <p class="text-center mt-4 text-lg">{{ description }}</p>
     <div class="flex justify-center">
       <UButton
         class="mt-4 dark:bg-gray-300 dark:hover:bg-gray-200"
-        size="lg"
+        size="xl"
         color="black" variant="solid"
         :ui="{
           font: 'font-semibold'
         }"
         :label="'Contact Me'"
         :icon="'i-line-md-email'"
+        to="mailto:crawfordteshane@gmail.com"
+        target="_blank"
       />
       <UButton
-        class="mt-4 ml-4 dark:bg-gray-700/10 dark:hover:bg-gray-700/20"
-        size="lg"
+        class="mt-4 ml-4 "
+        size="xl"
         color="gray" variant="solid"
         :label="'My GitHub'"
         :icon="'i-line-md-github'"
+        to="https:github.com/TeshaneCrawford"
+        target="_blank"
       />
     </div>
   </section>
