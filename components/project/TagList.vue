b31d33efc6bd2c2324e991d0d3f51f3d449cf5a9
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-04-24 18:45:20 -0500

    Add taglist for project component

---
+++ components/project/TagList.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+defineProps({
+  tags: {
+    type: Array as PropType<string[]>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <ul class="tags">
+    <li v-for="tag in tags" :key="tag">
+      <NuxtImg :src="`/img/${tag}.svg`" :alt="tag" />
+    </li>
+  </ul>
+</template>
+
+<style lang="postcss" scoped>
+.tags {
+  display: flex;
+  gap: 0.5rem;
+  padding: 0;
+  list-style: none;
+
+  NuxtImg {
+    display: block;
+    height: 1rem;
+    width: auto;
+  }
+}
+</style>
