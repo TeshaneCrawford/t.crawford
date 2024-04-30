22abf4ecc8370bae6fb3ee484290f363578164eb
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-04-30 11:47:03 -0500

    Add octokit package to package.json and update types/project.ts and server/api/repos.ts

--- types/project.ts
+++ types/project.ts
@@ -1,8 +1,22 @@
-export interface ProjectCard {
-  title: string
-  description: string
-  // image: string
-  link: string
-  source: string
-  tags: string[]
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+import type { Endpoints } from '@octokit/types';
+
+export interface JsonParsedContent<T> extends ParsedContent {
+  body: T
 }
+export interface Project {
+    name: string
+    description: string
+    icon?: string
+    webLink: string
+    githubUrl?: string
+    tags: { name: string, icon: string }[]
+}
+
+export interface ProjectList extends ParsedContent {
+    name: string
+    projects: Project[]
+}
+
+export type Repo = Endpoints['GET /user/repos']['response']['data'][number]
+export type User = Endpoints['GET /user']['response']['data']
