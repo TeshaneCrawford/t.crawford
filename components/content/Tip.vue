3ca94ff7370f784a8c057c2c9b38c335594d7e1e
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 18:22:23 -0500

    Update Note.vue component to use AppAlert and ContentSlot

--- components/content/Tip.vue
+++ components/content/Tip.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: content/Tip
-  </div>
+  <AppAlert title="Tip">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </AppAlert>
 </template>
 
 <style scoped></style>
