2b0ecc71daf08eb2e86b7e2d9c9b8323c07d45c1
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 12:41:31 -0500

    Refactor search functionality and update MiniSearch configuration

--- server/api/search.txt.ts
+++ server/api/search.txt.ts
@@ -1,36 +1,35 @@
-// import MiniSearch from 'minisearch'
-// import { serverQueryContent } from '#content/server'
+import MiniSearch from 'minisearch'
+import { serverQueryContent } from '#content/server'
 
-// export default defineEventHandler(async (event) => {
-//   const files = await serverQueryContent(event).find()
+export default defineEventHandler(async (event) => {
+  const files = await serverQueryContent(event).find()
 
-//   // Only works for MD
-//   const sections = (await Promise.all(
-//     files
-//       .filter(file => file._extension === 'md' && !file?._draft && !file?.empty)
-//       .map(page => splitPageIntoSections(page)),
-//   ))
-//     .flat()
+  const sections = (await Promise.all(
+    files
+      .filter(file => !file?._draft && !file?.empty && !file?._partial)
+      // TODO: Related to public.learn flag
+      .filter(file => !file?._path?.startsWith('/learn'))
+      .map(page => splitPageIntoSections(page)),
+  ))
+    .flat()
 
-//   // Add an option to enable index
-//   const miniSearch = new MiniSearch({
-//     fields: ['title', 'titles'],
-//     storeFields: ['title', 'titles'],
-//     searchOptions: {
-//       prefix: true,
-//       fuzzy: 0.2,
-//       boost: {
-//         title: 4,
-//         content: 2,
-//         titles: 1,
-//       },
-//     },
-//   })
+  const miniSearch = new MiniSearch({
+    fields: ['title', 'titles', 'text'],
+    storeFields: ['title', 'titles', 'text', 'level'],
+    searchOptions: {
+      prefix: true,
+      fuzzy: 0.2,
+      boost: {
+        title: 4,
+        text: 2,
+        titles: 1,
+      },
+    },
+  })
 
-//   // Index the documents
-//   miniSearch.addAll(sections)
+  // Index the documents
+  miniSearch.addAll(sections)
 
-//   setHeader(event, 'Content-Type', 'text/plain')
-//   // Send the index to the client
-//   return JSON.stringify(miniSearch)
-// })
+  setResponseHeader(event, 'Content-Type', 'text/plain')
+  return JSON.stringify(miniSearch)
+})
