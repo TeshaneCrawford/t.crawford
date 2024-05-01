79732114f9c35bb41266ccfeb239d8a0253d885e
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 18:16:24 -0500

    Update ProseCodeInline.vue component to use inline code styling and remove unnecessary div

--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: content/ProseCodeInline
-  </div>
+  <code class="mx-1px px-[0.375rem] py-[0.2rem] rounded-md font-mono font-normal bg-primary/30 dark:bg-primary/40 before:content-[''] after:content-['']">
+    <slot />
+  </code>
 </template>
 
 <style scoped></style>
