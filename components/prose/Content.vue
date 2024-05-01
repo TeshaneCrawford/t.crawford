df12947d503d922b150bb92db80ff164b27771e9
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 11:30:24 -0500

    Refactor Nav.vue component to remove unused code and update navigation structure

---
+++ components/prose/Content.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="prose prose-zinc dark:prose-invert max-w-none">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.prose :deep(:first-child) {
+  margin-top: 0;
+}
+.prose :deep(:last-child) {
+  margin-bottom: 0;
+}
+</style>
