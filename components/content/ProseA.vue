4f0cd1db061a8b24548db8a4572b6aed157af3e0
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 18:15:35 -0500

    Update ProseA.vue component to use NuxtLink and add computed property for rel attribute

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -1,9 +1,38 @@
-<script lang="ts" setup></script>
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  target?: string
+  rel?: string
+}>()
+
+const whiteList = [
+  'teshaneccrawford.live',
+  'nuxt.com',
+  'github.com',
+  'typescriptlang.org',
+  'vuejs.org',
+  'unocss.dev',
+  'tailwindcss.com',
+  'vitejs.dev',
+  'angular.dev'
+]
+
+const rel = computed(() => {
+  if (whiteList.some(domain => props.href.includes(domain)))
+    return 'noopener'
+
+  return 'noopener noreferrer'
+})
+</script>
 
 <template>
-  <div>
-    Component: content/ProseA
-  </div>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    :rel="rel"
+  >
+    <slot />
+  </NuxtLink>
 </template>
 
 <style scoped></style>
