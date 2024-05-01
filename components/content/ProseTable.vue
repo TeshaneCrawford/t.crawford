5c3bcb6143db4a01b970dff062035629ef18a9de
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 18:17:53 -0500

    Update ProseTable.vue component to use table element and adjust styling

--- components/content/ProseTable.vue
+++ components/content/ProseTable.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: content/ProseTable
+  <div m="y-8" overflow="x-auto" max-w="full">
+    <table class="gray-scrollbar">
+      <slot />
+    </table>
   </div>
 </template>
 
