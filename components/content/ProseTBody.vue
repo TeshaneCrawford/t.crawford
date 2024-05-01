99186c62641216d1b70cc08058e60966c88ed5c0
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-01 18:18:20 -0500

    Update ProseTBody.vue component to use table element and adjust styling

--- components/content/ProseTBody.vue
+++ components/content/ProseTBody.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: content/ProseTBody
-  </div>
+  <tbody>
+    <slot />
+  </tbody>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(td) {
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+}
+
+:deep(tr:nth-child(2n)) {
+  background-color: theme('colors.zinc.300 / 10%');
+}
+</style>
