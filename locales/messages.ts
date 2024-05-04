cef6e50584a32b9e0466de06211208326ef931cf
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-03 20:27:30 -0500

    refactor: Import global and project messages for i18n configuration

--- locales/messages.ts
+++ locales/messages.ts
@@ -1,12 +1,22 @@
 import en_availability from './en/availability.json'
 import fr_availability from './fr/availability.json'
 
+import en_global from './en/global.json'
+import fr_global from './fr/global.json'
+
+import en_projects from './en/projects.json'
+import fr_projects from './fr/projects.json'
+
 const messages = {
   en: {
     availability: en_availability,
+    global: en_global,
+    projects: en_projects,
   },
   fr: {
     availability: fr_availability,
+    global: fr_global,
+    projects: fr_projects,
   },
 }
 
