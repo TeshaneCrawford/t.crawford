419f7abfda63fe05a687c6f8fff6fc1e9e508674
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-03 20:09:38 -0500

    feat: Import messages from locales for i18n configuration

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,9 +1,9 @@
-
+import messages from '~/locales/messages'
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
   fallbackLocale: 'en',
   availableLocales: ['en', 'fr'],
-  // messages: messages,
+  messages: messages,
 }))
---
+++ locales/messages.ts
@@ -0,0 +1,13 @@
+import en_availability from './en/availability.json'
+import fr_availability from './fr/availability.json'
+
+const messages = {
+  en: {
+    availability: en_availability,
+  },
+  fr: {
+    availability: fr_availability,
+  },
+}
+
+export default messages
