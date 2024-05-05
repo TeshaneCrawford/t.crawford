0d971e86fbd048df22c2d6d9907e49486ba6fa1a
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-04 21:28:01 -0500

    refactor: Update tailwind.config.ts to use CSS variables for colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,19 @@
 import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+// import plugin from 'tailwindcss/plugin'
+// import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette'
+
+// function withOpacity(variableName: string) {
+//   // @ts-expect-error This error is expected because the opacityValue parameter may be undefined.
+//   return ({ opacityValue }) => {
+//     if (opacityValue !== undefined) {
+//       return `rgba(var(${variableName}), ${opacityValue})`
+//     }
+//     return `rgb(var(${variableName}))`
+//   }
+// }
+
 export default {
   content: [],
   theme: {
@@ -20,7 +33,41 @@ export default {
           '950': '#0e335d',
         },
         darkSecondary: "#25282A",
-      }
+        main: 'var(--bg-main)',
+        // 'main-opacity': withOpacity('--bg-main-opacity'),
+        secondary: 'var(--bg-secondary)',
+        muted: 'var(--font-muted)',
+        placeholder: 'var(--font-placeholder)',
+      },
+      textColor: {
+        main: 'var(--font-primary)',
+        muted: 'var(--font-muted)',
+        placeholder: 'var(--font-placeholder)',
+        inverted: 'var(--font-inverted)',
+      },
+      borderColor: {
+        primary: 'var(--border-primary)',
+        'primary-hover': 'var(--border-primary-hover)',
+        muted: 'var(--border-muted)',
+        transparent: 'transparent',
+      },
+      borderWidth: {
+        sm: '1px',
+        md: '2px',
+      },
+      animation: {
+        marquee: 'marquee 30s linear infinite',
+      },
+
+      keyframes: {
+        marquee: {
+          to: { transform: 'translateX(-50%)' },
+        },
+      },
+
+      textShadow: {
+        sm: 'rgba(255, 255, 255, 0.35) 1px 1px 12px',
+      },
     },
     fontFamily: {
       sans: [
