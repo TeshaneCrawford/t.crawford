bf6f491f4f0aeaa0d61c3dd9b1ff28df26890d15
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-05 01:11:38 -0500

    refactor: Update tools.vue to adjust grid layout for better responsiveness

--- pages/tools.vue
+++ pages/tools.vue
@@ -8,10 +8,10 @@ const { data: doc } = await useAsyncData(route.path, () => queryContent(route.pa
     <div v-for="tool in doc?.toolsData" :key="tool.name">
       <h3 class="text-3xl font-medium"> {{ tool.title }} </h3>
       <div class="flex flex-col gap-14">
-        <div class="grid grid-cols-3 mt-5 gap-3 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xl:grid-cols-6">
+        <div class="grid grid-cols-2 mt-5 gap-3 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xl:grid-cols-6">
           <div
             v-for="data in tool.toolData" :key="data.name"
-            class="relative flex flex-col gap-3 items-center justify-center bg-gray-100/15 dark:bg-darkSecondary/15 shadow dark:shadow-md p-8  border border-transparent hover:border-gray-400 dark:hover:border-neutral-600 rounded-md transition-all lg:hover:!scale-110 active:!scale-80 hover:z-10 hover:shadow-lg hover:origin-center hover:text-black dark:hover:text-white"
+            class="relative flex flex-col gap-3 items-center justify-center bg-gray-100/15 dark:bg-darkSecondary/15 shadow dark:shadow-md p-8  border border-transparent hover:border-gray-400 dark:hover:border-neutral-600 rounded-md transition-all lg:hover:!scale-110 active:!scale-80 hover:z-10 hover:shadow-lg hover:origin-center hover:text-black dark:hover:text-white backdrop-blur-sm"
           >
             <NuxtLink
             :to="data.link"
