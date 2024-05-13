2846ebca1cdec31f5753da3cd3952034ccf23d2a
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-13 17:43:57 -0500

    refactor: Update faq/Group.vue to provide details context for FAQ items

--- components/faq/Group.vue
+++ components/faq/Group.vue
@@ -1,4 +1,28 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const container = ref()
+
+const activeItem = ref(-1)
+
+function setActive(index: number) {
+  if (activeItem.value === index) {
+    activeItem.value = -1
+  } else {
+    activeItem.value = index
+  }
+}
+
+const children = ref([])
+
+onMounted(() => {
+  children.value = Array.from(container.value.children)
+})
+
+provide('details-context', {
+  peers: children,
+  activeItem,
+  setActive,
+})
+</script>
 
 <template>
   <div>
