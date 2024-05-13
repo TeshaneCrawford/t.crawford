91209a5c8626f7a3d0b1bc35f3783a894b63e8a3
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-13 17:49:33 -0500

    refactor: Update faq/Item.vue to provide toggle functionality for FAQ items

--- components/faq/Item.vue
+++ components/faq/Item.vue
@@ -1,9 +1,46 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+defineProps({
+  as: {
+    type: String,
+    default: 'div',
+  },
+})
+
+const container = ref()
+
+interface Context {
+  peers: Ref<HTMLElement[]> | undefined;
+  activeItem: Ref<number>;
+  setActive: (index: number) => void;
+}
+
+const context = inject<Context>('details-context');
+
+const index = computed(() => {
+  return context?.peers?.value ? context.peers.value.indexOf(container.value) : -1
+})
+
+const isActive = computed(() => index.value === (context?.activeItem?.value ?? -1))
+
+const toggle = () => {
+  if (context) {
+    context.setActive(index.value)
+  }
+}
+
+provide('isActive', isActive)
+</script>
 
 <template>
-  <div>
-    Component: faq/Item
-  </div>
+  <component
+    :is="as"
+    ref="container"
+  >
+    <slot
+      :toggle
+      :is-active
+    />
+  </component>
 </template>
 
 <style scoped></style>
