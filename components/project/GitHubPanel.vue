a0efe7fb994c266421286c946c283dabaab70a00
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-05-27 14:35:40 -0500

    refactor: Improve styling and responsiveness of GitHubPanel and GitHubRepo components

--- components/project/GitHubPanel.vue
+++ components/project/GitHubPanel.vue
@@ -9,8 +9,8 @@ defineProps<{
 
 <template>
   <div v-if="data.length">
-    <h4 class="py-4">{{ label }}</h4>
-    <div grid="~ cols-1 md:cols-2 lg:cols-3 gap-4">
+    <h4 class="py-4 font-semibold">{{ label }}</h4>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <ProjectGitHubRepo v-for="repo in data" :key="repo.id" :repo="repo" />
     </div>
   </div>
--- components/project/GitHubRepo.vue
+++ components/project/GitHubRepo.vue
@@ -12,12 +12,12 @@ defineProps<{
 </script>
 
 <template>
-  <article class="bg-black bg-opacity-0 dark:bg-white dark:bg-opacity-0 rounded-lg p-4 text-xs shadow-sm transition hover:bg-gray hover:bg-opacity-3 dark:hover:bg-white dark:hover:bg-opacity-3">
+  <article class="border bg-white bg-opacity-1 dark:bg-gray-600 dark:bg-opacity-0 rounded-lg p-4 text-xs shadow-sm transition hover:bg-gray-50/90 hover:bg-opacity-3 dark:hover:bg-black/10 dark:hover:bg-opacity-3">
     <NuxtLink :to="repo.homepage || repo.html_url" target="_blank" class="flex flex-col gap-3 h-full no-underline">
       <h5 class="m-0 font-semibold text-sm flex gap-1">
         <Icon name="i-ri-git-repository-line" />
         <span>{{ repo.name }}</span>
-        <span :class="repo.is_template ? 'dark-text-yellow-500 text-blue-500' : 'dark-text-teal-500 text-red-500'">
+        <span :class="repo.is_template ? 'dark:text-yellow-500 text-blue-500' : 'dark:text-teal-500 text-red-500'">
           {{ repo.private ? 'Private' : 'Public' }}
           {{ repo.is_template ? 'Template' : '' }}
         </span>
@@ -25,17 +25,17 @@ defineProps<{
       <p class="m-0 flex-1">{{ repo.description }}</p>
       <p class="m-0 flex gap-4">
         <span v-if="repo.language" class="flex gap-1">
-          <div class="h-3 w-3 inline-flex rounded-full" :style="{ backgroundColor: getLanguageColor(repo.language) }"/>
+          <div class="h-3 w-3 rounded-full" :style="{ backgroundColor: getLanguageColor(repo.language) }"/>
           {{ repo.language }}
         </span>
         <NuxtLink v-if="repo.stargazers_count" target="_blank" class="decoration-inherit" :to="`https://github.com/${repo.full_name}/stargazers`">
           <span class="flex gap-1">
-            <Icon name="i-ri-star-line" />
+            <UIcon class="icon" name="i-ri-star-line" />
             {{ repo.stargazers_count }}
           </span>
         </NuxtLink>
         <NuxtLink v-if="repo.forks_count" target="_blank" class="decoration-inherit flex gap-1" :to="`https://github.com/${repo.full_name}/network/members`">
-          <Icon name="i-ri-git-fork-line" />
+          <UIcon name="i-ri-git-fork-line" />
           {{ repo.forks_count }}
         </NuxtLink>
       </p>
@@ -44,4 +44,8 @@ defineProps<{
 </template>
 
 
-<style scoped></style>
+<style scoped>
+.icon {
+  font-size: 1rem;
+}
+</style>
