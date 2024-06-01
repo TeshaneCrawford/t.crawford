e22187918188f50765b8f681934e2113c96f6655
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-06-01 16:09:44 -0500

    feat: Add ContactSection component to home page

---
+++ components/home/ContactSection.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <HomeSection
+  :title="'Connect with me'"
+  class="mb-0"
+  >
+    <p class="">
+      I'm always open to new opportunities and collaborations. Feel free to reach out to me.
+    </p>
+    <ChatContactForm />
+  </HomeSection>
+</template>
+
+<style scoped></style>
