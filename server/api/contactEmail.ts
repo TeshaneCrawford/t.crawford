c97b6786d9fdec8413f40cd709a8325aab1e6589
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-06-03 11:30:16 -0500

    feat: Add email functionality to contactEmail API endpoint

--- server/api/contactEmail.ts
+++ server/api/contactEmail.ts
@@ -1,3 +1,26 @@
-export default defineEventHandler((event) => {
-  return 'Hello contactEmail'
+import { Resend } from 'resend';
+import type { H3Event } from 'h3';
+import type { ContactMail } from '~/types/mail';
+
+const resend = new Resend(process.env.NUXT_RESEND_API_KEY)
+
+export default defineEventHandler(async (event: H3Event) => {
+  try {
+    const body = (await readBody(event)) as ContactMail
+    const { name, email, message } = body
+    return await resend.emails.send({
+      from: 'Teshane Crawford <crawfordteshane@gmail.com>',
+      to: [''],
+      subject: 'Contact Form Submission',
+      html: `
+        <h1>Contact Form Submission</h1>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong> ${message}</p>
+        `,
+    })
+  }
+  catch (error) {
+    return error
+  }
 })
