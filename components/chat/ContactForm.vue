85eca21c0da6d740e2b2de89bfeea837f6835764
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-06-29 11:46:48 -0500

    refactor: Update ContactForm.vue to use separate variables for form fields

--- components/chat/ContactForm.vue
+++ components/chat/ContactForm.vue
@@ -21,6 +21,8 @@
 			state.status = 'pending'
 			if (!turnstileToken.value) throw new Error('No turnstile token')
 
+			const { name, email, message } = state.form; // Declare 'email' variable here
+
 			await $fetch('/api/contactEmail', {
 				method: 'POST',
 				body: {
@@ -49,10 +51,10 @@
 				<h2 class="H3Like">Get in touch with me</h2>
 				<div class="flex gap-6 flex-col mt-8">
 					<div class="flex flex-col md:flex-row gap-6">
-						<input v-model="name" type="text" placeholder="Name" required class="UIInput dark:bg-transparent dark:border" >
-						<input v-model="email" type="email" placeholder="Email" required class="UIInput dark:bg-transparent dark:border" >
+						<input v-model="state.form.name" type="text" placeholder="Name" required class="UIInput dark:bg-transparent dark:border" >
+						<input v-model="state.form.email" type="email" placeholder="Email" required class="UIInput dark:bg-transparent dark:border" >
 					</div>
-					<textarea v-model="message" placeholder="Message" required rows="5" class="UIInput dark:bg-transparent dark:border" />
+					<textarea v-model="state.form.message" placeholder="Message" required rows="5" class="UIInput dark:bg-transparent dark:border" />
 				</div>
 
 				<div class="flex justify-between flex-wrap gap-8 mt-8 mb-4">
