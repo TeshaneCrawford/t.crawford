50f843d3cad131d2b32a2990e5037f35c6b593a2
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-16 21:46:32 -0500

    Add static render component to render markdown components statically

---
+++ app/components/StaticMarkdownRender.server.ts
@@ -0,0 +1,17 @@
+import { ContentRendererMarkdown } from '#components'
+
+export default defineComponent({
+  props: {
+    path: String,
+  },
+  async setup(props) {
+    if (import.meta.dev) {
+      const { data } = await useAsyncData(() =>
+        queryContent(props.path!).findOne()
+      )
+      return () => h(ContentRendererMarkdown, { value: data.value! })
+    }
+    const value = await queryContent(props.path!).findOne()
+    return () => h(ContentRendererMarkdown, { value })
+  },
+})
