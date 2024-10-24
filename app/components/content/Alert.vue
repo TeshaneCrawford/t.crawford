ae475d2d9245e79ad474dec9f2870b7276d5c4f6
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-24 14:53:51 -0500

    Add custom prose components for nuxt content

---
+++ app/components/content/Alert.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+type Type = 'info' | 'warning' | 'danger'
+
+defineProps<{
+  type: Type
+}>()
+
+const typeClasses: Record<Type, string> = {
+  info: 'border-blue-300/40 dark:border-blue-700/40 bg-blue-200/20 dark:bg-blue-700/10 dark:text-blue-50 text-blue-950',
+  warning: 'border-yellow-300/40 dark:border-yellow-700/40 bg-yellow-200/20 dark:bg-yellow-700/10 dark:text-yellow-50 text-yellow-950',
+  danger: 'border-red-300/40 dark:border-red-700/40 bg-red-200/20 dark:bg-red-700/10 dark:text-red-50 text-red-950',
+}
+</script>
+
+<template>
+  <div class="alert my-8 border rounded-md px-4 py-4 text-sm" :class="typeClasses[type]">
+    <ContentSlot :use="$slots.default" />
+  </div>
+</template>
+
+<style scoped>
+.alert :deep(*:first-child) {
+  margin-top: 0;
+}
+
+.alert :deep(*:last-child) {
+  margin-bottom: 0;
+}
+</style>
---
+++ app/components/content/ProseBlockquote.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <blockquote class="mt-3 border-l-8 text-sm font-mono italic">
+    <slot />
+  </blockquote>
+</template>
+
+<style scoped>
+</style>
