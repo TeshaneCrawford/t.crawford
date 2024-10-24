7dfe2c8ba6fcc8bfb056a191eb2088e630557775
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-24 14:52:53 -0500

    Title for heading

---
+++ app/components/app/Title.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  text?: string
+}>()
+</script>
+
+<template>
+  <h1 class="text-3xl text-neutral-8 font-bold tracking-tight sm:text-[2.5rem] dark:text-neutral-1">
+    <slot>{{ text }}</slot>
+  </h1>
+</template>
+
+<style scoped></style>
