db9c35029a83ff382a5202878b507a90f14ba3d3
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-24 14:47:42 -0500

    Styling - Add global styling for app

--- uno.config.ts
+++ uno.config.ts
@@ -14,29 +14,36 @@ export default defineConfig({
     filesystem: ['content/**/*.md'],
   },
   shortcuts: [
-    ['prose-container', 'prose max-w-4xl!']
+    ['prose-container', 'prose max-w-4xl!'],
+
+    [/^badge-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
+    [/^badge-xs-(.*)$/, ([, c]) => `badge-${c} text-xs px2 py0.5`],
+    [/^badge-sm-(.*)$/, ([, c]) => `badge-${c} text-sm px3 py0.6`],
+    [/^badge-lg-(.*)$/, ([, c]) => `badge-${c} px3 py0.8`],
+    [/^badge-square-(.*)$/, ([, c]) => `badge-${c} w-7 h-7 text-lg font-200 flex flex-none items-center justify-center`],
   ],
   theme: {
     fontFamily: {
       sans: 'DM Sans',
       mono: 'DM Mono',
-      serif: 'DM Serif Display',
+      serif: 'Playwrite GB S',
+      cursive: 'Satisfy',
       dank: 'dank'
     },
     colors: {
-        text: {
-          primary: 'var(--text-primary)',
-          secondary: 'var(--text-secondary)',
-        },
-        bg: {
-          primary: 'var(--background-primary)',
-          secondary: 'var(--background-secondary)',
-        },
-        accent: 'var(--accent)',
-        scrollbar: 'var(--scrollbar)',
-        'scrollbar-hover': 'var(--scrollbar-hover)',
-        link: 'var(--link-color)',
-        'link-hover': 'var(--link-hover-color)',
+      text: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+      },
+      bg: {
+        primary: 'var(--background-primary)',
+        secondary: 'var(--background-secondary)',
+      },
+      accent: 'var(--accent)',
+      scrollbar: 'var(--scrollbar)',
+      'scrollbar-hover': 'var(--scrollbar-hover)',
+      link: 'var(--link-color)',
+      'link-hover': 'var(--link-hover-color)',
     },
     fontSize: {
       base: 'var(--font-size-base)',
@@ -61,23 +68,24 @@ export default defineConfig({
     }),
     presetTypography({
       selectorName: "prose",
-			cssExtend: {
-				"p,a,li,ul,ol": {
-					color: "#eeeee",
+      cssExtend: {
+        "p,a,li,ul,ol": {
+          color: "#eeeee",
           'line-height': "2"
-				},
+        },
         a: {
           "text-decoration": "none"
         },
-        'a:hover': {
-          color: '#f43f5e',
-          "text-decoration": "underline",
-          "text-underline-offset": "8px"
-        },
+        // 'a:hover': {
+        //   color: '#f43f5e',
+        //   "text-decoration": "underline",
+        //   "text-underline-offset": "8px"
+        // },
         // 'a:visited': {
         //   color: '#14b8a6',
         // },
-			},
+
+      },
     }),
     presetWebFonts({
       fonts: {
