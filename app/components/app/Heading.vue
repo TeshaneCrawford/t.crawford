56a8fdaf93804792e31cb8ff64c622edbd0b78dd
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-24 14:52:11 -0500

    Add title heading component for app

---
+++ app/components/app/Heading.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  description?: string
+}>()
+</script>
+
+<template>
+  <div class="lg:px-0 md:px-8">
+    <div class="">
+      <div class="mx-auto max-w-2xl flex flex-col gap-16 lg:max-w-5xl">
+        <div
+          v-if="title || description || $slots.title
+            || $slots.description"
+          class="flex flex-col gap-4"
+        >
+          <AppTitle>
+            <slot name="title">
+              {{ title }}
+            </slot>
+          </AppTitle>
+          <p class="max-w-4xl text-sm text-neutral-5 font-dank italic sm:text-base dark:text-neutral-4">
+            <slot name="description">
+              {{ description }}
+            </slot>
+          </p>
+        </div>
+
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
