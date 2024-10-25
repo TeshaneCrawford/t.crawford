c805ab7259fabb381e69ab29d20fecb96246efdb
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-25 08:39:49 -0500

    Improve seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -150,6 +150,10 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
       htmlAttrs: { lang: 'en' },
       title: 'Teshane Crawford',
     },
