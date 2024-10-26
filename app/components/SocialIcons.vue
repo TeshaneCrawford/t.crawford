c6034f619dd6504136f6ff410b1121e84069f079
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-26 18:40:01 -0500

    chore(fix): Links url in social icons

--- app/components/SocialIcons.vue
+++ app/components/SocialIcons.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="">
-    <NuxtLink target="_blank" to="https://twitter.com/_D_Shybrid" title="Twitter" class="group">
+    <NuxtLink target="_blank" to="https://twitter.com/TeshanCrawford" title="Twitter" class="group">
       <Icon name="i-hugeicons:new-twitter" hover-text-neutral-600 alt="Twitter logo" />
     </NuxtLink>
     <NuxtLink target="_blank" to="https://github.com/TeshanCrawford" title="GitHub" class="group">
@@ -14,7 +14,7 @@
     <NuxtLink target="_blank" to="https://discord.gg/4Q3avDs" title="Discord" class="group">
       <Icon name="i-hugeicons-discord" hover-text-neutral-600 alt="Discord logo" />
     </NuxtLink>
-    <NuxtLink target="_blank" to="https://www.instagram.com/teshanecrawford/" title="Instagram" class="group">
+    <NuxtLink target="_blank" to="https://www.instagram.com/crawfordteshane/" title="Instagram" class="group">
       <Icon name="i-hugeicons-instagram" hover-text-neutral-600 alt="Instagram logo" />
     </NuxtLink>
     <NuxtLink target="_blank" to="https://www.threads.net/@teshanecrawford" title="Threads" class="group">
