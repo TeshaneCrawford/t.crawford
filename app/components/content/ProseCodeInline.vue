c1a5f430cf46929607addcec3592deebb4f48800
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-20 02:31:25 -0500

    feat: enhance ProseCodeInline component with role attribute and transition effects

--- app/components/content/ProseCodeInline.vue
+++ app/components/content/ProseCodeInline.vue
@@ -1,5 +1,8 @@
 <template>
-  <code class="mx-1px rounded-md bg-sky-50 px-[0.375rem] py-[0.2rem] text-sky-500 font-400 font-mono dark:bg-sky-950 dark:text-sky-400 after:content-[''] before:content-['']">
+  <code 
+    role="text"
+    class="mx-1px rounded-md bg-sky-50 px-[0.375rem] py-[0.2rem] text-sky-500 font-400 font-mono transition-colors duration-200 dark:bg-sky-950 dark:text-sky-400 after:content-[''] before:content-['']"
+  >
     <slot />
   </code>
 </template>
@@ -9,6 +12,7 @@ a code {
   border-width: 1px;
   border-style: dashed;
   border-color: theme('colors.sky.500');
+  transition: all 200ms ease;
 }
 
 a:hover code {
