813b54220fa5c69e5a875e4e34a7cc98a2074c7b
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-12-20 22:38:43 -0500

    feat: add ProseHeading component for enhanced heading navigation and smooth scrolling

---
+++ app/components/prose/Heading.vue
@@ -0,0 +1,42 @@
+<!--
+  Credit: Adapted from https://github.com/zyyv/zyyv.dev/blob/main/app/components/ProseHeading.vue
+-->
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports'
+import { proseHeadingClick } from '~/utils/proseHeading'
+
+// @unocss-skip-start
+const props = defineProps<{
+  id?: string
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}>()
+// @unocss-skip-end
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && (
+  (typeof headings?.anchorLinks === 'boolean' && headings?.anchorLinks === true)
+  || (typeof headings?.anchorLinks === 'object' && headings?.anchorLinks?.[props.tag])),
+)
+</script>
+
+<template>
+  <component :is="props.tag" :id="props.id" class="group text pr text-op-90">
+    <a
+      v-if="props.id && generate"
+      :href="`#${props.id}`"
+      text-inherit
+      @click="proseHeadingClick($event, props.id)"
+    >
+      <slot />
+    </a>
+    <slot v-else />
+    <span
+      v-if="props.id && generate"
+      pa fcc size-5.5 bg-primary:10 rd pyc left--7
+      group-hover:op-100 trans op-0 cursor-pointer
+      @click="proseHeadingClick($event, props.id)"
+    >
+      <Icon name="i-hugeicons-link-01" text-sm />
+    </span>
+  </component>
+</template>
---
+++ app/utils/proseHeading.ts
@@ -0,0 +1,12 @@
+export async function proseHeadingClick(event: MouseEvent, id: string) {
+  event.preventDefault()
+  window.history.replaceState(null, '', `#${id}`)
+
+  const element = document.getElementById(id)
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop - 86, // header height + 16px
+      behavior: 'smooth',
+    })
+  }
+}
