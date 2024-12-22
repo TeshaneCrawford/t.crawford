fd45248223e158c0198f4ec62ace529ec07cd0dd
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-12-22 14:51:31 -0500

    fix: add missing newlines at the end of various type definition files

--- types/github.ts
+++ types/github.ts
@@ -1,14 +1,14 @@
 export interface User {
-  name: string;
-  username: string;
-  avatar: string;
-  bio: string | null;
-  followers: number;
-  following: number;
-  public_repos: number;
-  location: string | null;
+  name: string
+  username: string
+  avatar: string
+  bio: string | null
+  followers: number
+  following: number
+  public_repos: number
+  location: string | null
 }
 
 export interface GitHubData {
-  user: User;
+  user: User
 }
--- types/project.ts
+++ types/project.ts
@@ -2,56 +2,56 @@ import type { ParsedContentv2 } from '@nuxt/content'
 import type { Endpoints } from '@octokit/types'
 
 export interface Project {
-    name: string
-    description: string
-    icon?: string
-    webLink: string
-    githubUrl?: string
-    tags: { name: string, icon: string }[]
+  name: string
+  description: string
+  icon?: string
+  webLink: string
+  githubUrl?: string
+  tags: { name: string, icon: string }[]
 }
 
 export interface ProjectList extends ParsedContentv2 {
-    name: string
-    projects: Project[]
+  name: string
+  projects: Project[]
 }
 
 export type OctokitRepo = Endpoints['GET /users/{username}/repos']['response']['data'][number]
 
 export interface Repo {
+  id: number
+  node_id: string
+  name: string
+  full_name: string
+  private: boolean
+  owner: {
+    login: string
     id: number
     node_id: string
-    name: string
-    full_name: string
-    private: boolean
-    owner: {
-        login: string
-        id: number
-        node_id: string
-        avatar_url: string
-        url: string
-        html_url: string
-        type: string
-        site_admin: boolean
-    }
-    html_url: string
-    description: string | null
-    fork: boolean
+    avatar_url: string
     url: string
-    archived: boolean
-    disabled: boolean
-    topics: string[]
-    license?: {
-        key?: string
-        name?: string
-        url?: string | null
-        spdx_id?: string | null
-        node_id?: string
-    } | null
-    homepage: string | null | undefined;
-    stargazers_count: number | undefined;
-    is_template?: boolean | undefined;
-    language: string | null | undefined;
-    forks_count: number | undefined;
+    html_url: string
+    type: string
+    site_admin: boolean
+  }
+  html_url: string
+  description: string | null
+  fork: boolean
+  url: string
+  archived: boolean
+  disabled: boolean
+  topics: string[]
+  license?: {
+    key?: string
+    name?: string
+    url?: string | null
+    spdx_id?: string | null
+    node_id?: string
+  } | null
+  homepage: string | null | undefined
+  stargazers_count: number | undefined
+  is_template?: boolean | undefined
+  language: string | null | undefined
+  forks_count: number | undefined
 }
 
 export type User = Endpoints['GET /user']['response']['data']
