e1f4a08d5d59e5b7424e75717ed1b8d264747901
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-12-22 19:05:29 -0500

    fix: remove unnecessary line breaks in ViewTransition plugin for cleaner code

--- app/plugins/ViewTransition.client.ts
+++ app/plugins/ViewTransition.client.ts
@@ -1,12 +1,12 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('page:finish', () => {
-    if (!document.startViewTransition) return;
+    if (!document.startViewTransition) return
 
-    const route = useRoute();
+    const route = useRoute()
     if (route.name === 'blog-article') {
       document.startViewTransition(() => {
         // This callback is called when the DOM is updated
-      });
+      })
     }
-  });
-});
+  })
+})
